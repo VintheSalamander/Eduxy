a48a363535976115a303d9da9298a0842ebed42e
VintheSalamander <vineetnihalani01@gmail.com>
2023-03-11 22:51:21 +0000

    Pause button and UI
    Replaced the pause prefab with a UI one, adding a pause button

--- Assets/Scripts/PauseMenu/Pause.cs
+++ Assets/Scripts/PauseMenu/Pause.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
     public bool paused;
     public GameObject PauseMenu;
+    public GameObject pauseBut;
+
     // Update is called once per frame
     void Update()
     {
         if(paused){
             PauseMenu.SetActive(true);
+            pauseBut.SetActive(false);
             Time.timeScale = 0f;
         }else{
             PauseMenu.SetActive(false);
+            pauseBut.SetActive(true);
             Time.timeScale = 1f;
         }
         if(Input.GetKeyDown(KeyCode.Escape)){
